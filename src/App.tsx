258aeb565b1e2ef5854b85848a7e7847b1b1d058
Toyin <oluwatoyinogundare@gmail.com>
2020-04-14 22:21:39 -0600

    new services

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from './Components/Navigation'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>REACT DAY 4</h1> 
+    <Navigation />
+    </div>
+  );
+}
+
+export default App;
