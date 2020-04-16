63013192ee73ac17253e35df16218ea82cf7b00b
Toyin <oluwatoyinogundare@gmail.com>
2020-04-16 00:40:27 -0600

    updated files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,58 @@
 import React from 'react';
-import Navigation from './Components/Navigation'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Services from './Components/Services';
+import Contact from './Components/Contact';
+// import Navigation from './components/Navigation';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    <h1>REACT DAY 4</h1> 
-    <Navigation />
-    </div>
-  );
+class App extends React.Component {
+  componentWillUnmount ()
+  {
+      console.log( '"componentWillUnmount()" ran.' );
+  }
+  render()
+  {
+    return (
+      <div className="App">
+        <h1>React Day 4</h1>
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Homepage</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/services">Services</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+              {/* You can place other elements/classes/components/tags here too! */}
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/services">
+              <Services />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
